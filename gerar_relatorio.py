99364ef683d1ae0eb599cfbd35917a8018451efa
Otávio cortes <otavcort@gmail.com>
2025-04-29 21:34:37 -0300

    Atualizaçao

--- gerar_relatorio.py
+++ gerar_relatorio.py
@@ -1,7 +1,7 @@
 import pandas as pd  
 
 df = pd.read_csv('data.csv')  
-relatorio = df.describe().to_html() # Gera estatísticas em HTML   
+relatorio = df.describe().to_html() 
 
 with open('relatorio.html', 'w') as f:  
-    f.write(f"<h1>Relatório de Dados</h1>\n{relatorio}")  # Salva o HTML 
+    f.write(f"<h1>Relatório de Dados</h1>\n{relatorio}")
